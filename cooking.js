178360f18ad6520756a2cea3354922f39d3f1ad3
aquintana96 <47090413+aquintana96@users.noreply.github.com>
2019-04-25 00:43:35 -0700

    Initizial upload
    errything

---
+++ cooking.js
@@ -0,0 +1,378 @@
+// javascript for final Project
+// sets buttons and a search function that finds recipes with clicked
+// checked marks that give you the recipes that you want
+"use strict";
+(function(){
+  let fold; //function to get folderid
+  /**onload function, sets buttons and displays all recipes **/
+  window.onload = function() {
+      recipes();
+      document.getElementById("back").onclick = recipes;
+      document.getElementById("search").onclick = search; //init buttons
+      document.getElementById("post").onclick = post;
+      //start up for timer
+
+  };
+  /**function that searches once button is clicked**/
+  function search(){
+      let checked = getCheckedBoxes();
+      console.log(checked);
+
+        //gets the recipes from host
+        fetch("http://localhost:3000/?mode=check&title=null")
+            .then(checkStatus)
+            .then(function(responseText) {
+              let list = [];
+              let rep = JSON.parse(responseText);
+              //used to check if the item clicked is shown
+              for (let x = 0; x < rep.list.length; ++x){
+                let data = {"folder": rep.list[x].folder};
+                data["count"] = 0;
+                for (let i = 0; i < checked.length; ++i){
+                  if (rep.list[x].ingredients.includes(checked[i])){
+                    data["count"] += 1;
+                  }
+                  list[x] = data;
+                }
+              }
+              let fin = {"final": list};
+              //checks to see if checked is on there
+              if (fin.final.length <= 0){
+                  recipes();
+              }
+              else {
+              recipes2(fin);
+            }
+            })
+            .catch(function(error) {
+              console.log(error);
+        });
+
+    }
+    /**function that displays recipes with ingredients clicked**/
+    function recipes2(rep){
+      //hides all stuff not needed and shows what is needed
+      document.getElementById("craft").style.visibility ="hidden";
+      document.getElementById("third").style.visibility="hidden";
+      document.getElementById("inputs").style.visibility="visible";
+      document.getElementById("craft2").innerHTML = "";
+      document.getElementById("revtext").innerHTML = "";
+      let rec = document.getElementById("recipes");
+      rec.innerHTML = "";
+      //loops and remakes the divs for shown items
+            for(let i = 0; i < rep.final.length; ++i){
+              if (rep.final[i].count > 0){
+                let single = document.createElement("div");
+                let photo = document.createElement("img");
+                single.id = rep.final[i].folder;
+                photo.src = "recipes/"+ rep.final[i].folder +"/cover.jpg";
+                let para = document.createElement("p");
+                let temp = rep.final[i].folder.replace(/([a-z])([A-Z])/g, '$1 $2');
+                para.innerHTML = temp;
+                single.onmousedown = clickDown;
+                single.addEventListener("mouseover", expand);
+                single.addEventListener("mouseout", revert);
+                single.appendChild(photo);
+                single.appendChild(para);
+
+                rec.appendChild(single);
+              }
+            }
+    }
+
+    /**function that gets all checked boxes**/
+    function getCheckedBoxes() {
+    let check = document.getElementsByClassName("checkBox");
+    let temp = 0;
+    let ccheck = []; //makes them into an array and passes if it is checked
+    for (let i=0; i< check.length; i++) {
+       if (check[i].checked) {
+          ccheck[temp] = check[i].id.toLowerCase(); //sends checked name
+          temp += 1;
+       }
+    }
+    return ccheck;
+  }
+
+  /**posts to file once review is submitted**/
+  function post() {
+  let name = document.getElementById("name").value;
+  let comment = document.getElementById("message").value;
+  //sets the message to be sent to js server
+  console.log(fold);
+  const message = {name: name,
+                  folder: fold,
+                   comment: comment};
+  const fetchOptions = {
+    method : 'POST',
+    headers : {
+      'Accept': 'application/json',
+      'Content-Type' : 'application/json'
+    },
+    body : JSON.stringify(message)
+  };
+
+  console.log(message);
+  //url for posting to server takes name and comment
+  let url = "http://localhost:3000/";
+  fetch(url, fetchOptions)
+    .then(checkStatus)
+    .then(function(responseText) {
+      console.log(responseText);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+    //fetch to get updated file after posting t0 it
+    fetch("http://localhost:3000/?mode=reviews&title=" + fold)
+        .then(checkStatus)
+        .then(function(responseText) {
+          let c = document.getElementById("revtext");
+          c.innerHTML = "";
+          let list = JSON.parse(responseText);
+          for (let i = 0; i < list.reviews.length; i++){
+            let single = list.reviews[i];
+            let p = document.createElement("p");
+            p.innerHTML = single;
+            c.appendChild(p);
+          }
+        })
+        .catch(function(error) {
+          console.log(error);
+    });
+
+}
+
+  /**function that creates all the recipes shown**/
+  function recipes(){
+    //hides inner code divs from recipes clicked and turns them all on
+    document.getElementById("craft").style.visibility ="hidden";
+    document.getElementById("third").style.visibility="hidden";
+    document.getElementById("inputs").style.visibility="visible";
+    //resizes the divs to fit better
+    document.getElementById("inputs").style.width = 26 + "%";
+    // document.getElementById("inputs").style.position = "relative";
+    document.getElementById("reci").style.width = 55 + "%";
+    document.getElementById("craft2").innerHTML = "";
+    document.getElementById("revtext").innerHTML = "";
+    //fetch that call the recipe on javascript
+    let rec = document.getElementById("recipes");
+    rec.innerHTML = "";
+    fetch("http://localhost:3000/?mode=recipes&title=null")
+        .then(checkStatus)
+        .then(function(responseText) {
+          let rep = JSON.parse(responseText);
+          //forloop that runs and creats div, img, and paragraph for recipe
+          for(let i = 0; i < rep.recipes.length; ++i){
+            let single = document.createElement("div");
+            let photo = document.createElement("img");
+            single.id = rep.recipes[i].folder;
+            photo.src = "recipes/"+ rep.recipes[i].folder +"/cover.jpg";
+            let para = document.createElement("p");
+            para.innerHTML = rep.recipes[i].title;
+            single.onmousedown = clickDown;
+            single.addEventListener("mouseover", expand);
+            single.addEventListener("mouseout", revert);
+            single.appendChild(photo);
+            single.appendChild(para);
+            //appends it to rec div
+            rec.appendChild(single);
+          }
+        })
+        .catch(function(error) {
+          console.log(error);
+    });
+  }
+  /**function that exapnds the current recipe hoverd**/
+  function expand(){
+    this.style.width = 190 +"px";
+    this.style.height = 260 + "px"; //increase image and changes backgroundcolor
+    this.style.marginBottom = 15 + "px";
+    this.style.backgroundColor = "#FFD4FF";
+
+  }
+  /**function that reverts once expand once reipe isnt hoverd**/
+  function revert(){
+    this.style.width = 180 + "px";
+    this.style.height = 250 + "px";//decreases image and changes color
+    this.style.marginBottom = 25 + "px";
+    this.style.backgroundColor = "#FFE5FF";
+  }
+  /**function that goes once recipe is clicked**/
+  function clickDown(){
+    let craft = document.getElementById("craft2");
+    //makes visible and hides divs that are important
+    document.getElementById("craft").style.visibility="visible";
+    document.getElementById("third").style.visibility="visible";
+    document.getElementById("inputs").style.visibility="hidden";
+    // document.getElementById("inputs").style.position = "absolute";
+    document.getElementById("inputs").style.width = 6 + "%";
+    document.getElementById("reci").style.width = 75 + "%";
+    document.getElementById("recipes").innerHTML = "";
+    //goes throught and gets information for the recipe clicked
+    let temp = this.id.replace(/([a-z])([A-Z])/g, '$1 $2');
+    let single = document.createElement("h2");
+    fold = this.id;
+    single.innerHTML = temp;
+    let photo = document.createElement("img");
+    photo.src = "recipes/"+ this.id +"/cover.jpg";
+    craft.appendChild(single);
+    craft.appendChild(photo);
+    //all the fetches go and get necassery clicked items from json
+    fetch("http://localhost:3000/?mode=difficulty&title=" + this.id)
+        .then(checkStatus)
+        .then(function(responseText) {
+            let single = document.createElement("p");
+            single.innerHTML = "Difficulty: " + responseText;
+            craft.appendChild(single);
+        })
+        .catch(function(error) {
+          console.log(error);
+    });
+    fetch("http://localhost:3000/?mode=ingredients&title=" + this.id)
+        .then(checkStatus)
+        .then(function(responseText) {
+            let ingr = JSON.parse(responseText);
+            let box = document.createElement("div");
+            for(let i = 0; i < ingr.ingredients.length; ++i){
+            let single = document.createElement("li");
+            single.innerHTML = ingr.ingredients[i];
+            box.appendChild(single);
+          }
+          craft.appendChild(box);
+        })
+        .catch(function(error) {
+          console.log(error);
+    });
+    fetch("http://localhost:3000/?mode=servings&title=" + this.id)
+        .then(checkStatus)
+        .then(function(responseText) {
+          let temp = JSON.parse(responseText);
+          let single = document.createElement("p");
+          single.innerHTML = temp;
+          craft.appendChild(single);
+        })
+        .catch(function(error) {
+          console.log(error);
+    });
+    fetch("http://localhost:3000/?mode=time&title=" + this.id)
+        .then(checkStatus)
+        .then(function(responseText) {
+          let temp = JSON.parse(responseText);
+          let single = document.createElement("p");
+          single.innerHTML = temp.prep + temp.total;
+          craft.appendChild(single);
+        })
+        .catch(function(error) {
+          console.log(error);
+    });
+    fetch("http://localhost:3000/?mode=instructions&title=" + this.id)
+        .then(checkStatus)
+        .then(function(responseText) {
+          let ingr = JSON.parse(responseText);
+          let box = document.createElement("div");
+          for(let i = 0; i < ingr.instructions.length; ++i){
+            let single = document.createElement("p");
+            single.innerHTML = ingr.instructions[i];
+            box.appendChild(single);
+          }
+          craft.appendChild(box);
+        })
+        .catch(function(error) {
+          console.log(error);
+    });
+    fetch("http://localhost:3000/?mode=reviews&title=" + this.id)
+        .then(checkStatus)
+        .then(function(responseText) {
+          let c = document.getElementById("revtext");
+          c.innerHTML = "";
+          let list = JSON.parse(responseText);
+          for (let i = 0; i < list.reviews.length; i++){
+            let single = list.reviews[i];
+            let p = document.createElement("p");
+            p.innerHTML = single;
+            c.appendChild(p);
+          }
+        })
+        .catch(function(error) {
+          console.log(error);
+    });
+  }
+
+  // // function back(){
+  // //   document.getElementById("craft").style.visibility="hidden";
+  // //   document.getElementById("recipes").style.visibility="visible";
+  // // }
+  // /**function that starts the timer**/
+  // function startclick() {
+  //   let start = document.getElementById("start"); //sets buttons to a variable
+  //   let stop = document.getElementById("stop");
+  //   start.setAttribute( "class", "off" );  //turns the start button off
+  //   stop.setAttribute( "class", "on" ); //turns stop button off
+  //   start.disabled = true;
+  //   stop.disabled = false;
+  //   countDown();
+  // }
+  // /**Function that stops the timer **/
+  // function stopclick() {
+  //   let start = document.getElementById("start");
+  //   let stop = document.getElementById("stop");
+  //   start.setAttribute( "class", "on" );
+  //   stop.setAttribute( "class", "off" );
+  //   stop.disabled = true;
+  //   start.disabled = false;
+  //   clearInterval(timer);
+  // }
+  // /**function makes counter count down**/
+  // function countDown(){
+  //   let h = parseInt(document.getElementById("hour").value);
+  //   let m = parseInt(document.getElementById("minutes").value);
+  //   let s = parseInt(document.getElementById("seconds").value);
+  //   let clock = document.getElementById("timer");
+  //   let distance = h * 60 * 60 * 1000 + m * 60 * 1000 + s * 1000;
+  //   let temp = 1;
+  //   timer = setInterval(function() {
+  //     let unit = [];
+  //     //hours minutes seconds
+  //     unit[0] = Math.floor(distance / (1000 * 60 * 60));
+  //     unit[1] = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  //     unit[2] = Math.floor((distance % (1000 * 60)) / 1000);
+  //     console.log(Math.floor((distance % (1000 * 60)) / 1000));
+  //     for (let i = 0; i < unit.length; ++i){
+  //       if (unit[i] < 10){
+  //         unit[i] = "0" + unit[i];
+  //       }
+  //     }
+  //     console.log(unit[2]);
+  //     clock.innerHTML= "";
+  //     clock.innerHTML = unit[0] + ":"+ unit[1] + ":"
+  //      + unit[2];
+  //     if (distance == 0){
+  //       clock.innerHTML = "00:00:00";
+  //       clearInterval(timer);
+  //     }
+  //     else {
+  //       distance = distance - 1;
+  //     }
+  //   }, 1000);
+  // }
+
+
+
+    /**Checks for the errors**/
+    function checkStatus(response) {
+      if (response.status >= 200 && response.status < 300) {
+          return response.text();
+      }
+      else if (response.status == 410) { //for 410 error
+        return Promise.reject(new Error("410 No information could be found"));
+      }
+      else if (response.status == 404) { //for 404 error
+        return Promise.reject(new Error("404 can not find the page"));
+      }
+      else {
+       return Promise.reject(new Error(response.status+": "+response.statusText));
+      }
+    }
+
+})();
---
+++ cooking_service.js
@@ -0,0 +1,185 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers",
+               "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
+app.use(express.static('public'));
+
+
+/**function for reading off the files and returns a string**/
+function read_file(file_name, title) {
+	let info = 0;
+	try {
+	    info = fs.readFileSync("recipes/"+ title +"/"+file_name, 'utf8');
+	} catch(e) {
+	    console.log('Error:', e.stack);
+	}
+	return info;
+}
+
+	/**creates the JSON for the ingredients.txt**/
+ function build_ingredients(line){
+   line = line + "";
+   line = line.split("\n");
+   let data = {"ingredients":line};
+   return data;
+ }
+
+ /**creates the JSON for the instructions file**/
+function build_instructions(line){
+
+  line = line.split("\n");
+  let data = {"instructions":line};
+  return data;
+}
+
+/**creates the JSON for the time file**/
+function build_time(line){
+ line = line.split("\n");
+ let temp = line[0].split(":::");
+ let temp2 = temp[0] + ":" + temp[1];
+ let data = {"prep":temp2};
+ temp = line[1].split(":::");
+ temp2 = temp[0] + ":" + temp[1];
+ data["total"] = temp2;
+ return data;
+}
+
+/**Builds the list of every recipe in the recipe folder as a JSON**/
+ function build_recipes(){
+	 let rec = fs.readdirSync("recipes/");
+	 let i = 0;
+	 let list = [];
+   let temp = "";
+	 while(i < rec.length) {
+		 temp = rec[i].replace(/([a-z])([A-Z])/g, '$1 $2');
+     let data = {"title":temp};
+     data["folder"] = rec[i];
+		 list[i] = data;
+		 i++;
+	 }
+	 let book = {"recipes":list};
+	 return book;
+ }
+  /**gets all the ingredients**/
+  function build_check(){
+ 	 let rec = fs.readdirSync("recipes/");
+ 	 let i = 0;
+ 	 let list = [];
+    let temp;
+    console.log("k");
+ 	 while(i < rec.length) {
+     temp = read_file("ingredients.txt", rec[i]);
+     let temp1 = temp.replace(/(\r\n|\n|\r)/gm," ");
+     temp = temp1.split(" ");
+     let data = {"folder": rec[i]};
+      data["ingredients"] = temp;
+ 		 list[i] = data;
+ 		 i++;
+ 	 }
+ 	 let rep = {"list":list};
+ 	 return rep;
+  }
+
+ /**Builds the JSON for all the messages**/
+  function build_Message(info){
+   info = info.split("\n");
+ 	 let final = {"reviews":info};
+ 	 return final;
+  }
+
+console.log('web service started');
+app.get('/', function (req, res) {
+	res.header("Access-Control-Allow-Origin", "*");
+
+  let title = req.query.title;
+  let mode = req.query.mode;
+
+  let line = "";
+  let temp;
+
+ //an error case that isnt required
+  if(title == undefined || mode == undefined) {
+    res.status(400);
+    res.send("Missing required parameters");
+  }
+
+//Switch statement for all posible the mode values in the url
+switch (mode) {
+  case "difficulty":
+    temp = read_file("difficulty.txt",title);
+    line = temp.replace(/(\r\n|\n|\r)/gm,"");
+  break;
+  case "check":
+  line = build_check();
+  break;
+  case "ingredients":
+    temp = read_file("ingredients.txt",title);
+    line = build_ingredients(temp);
+  break;
+  case "instructions":
+  temp = read_file("instructions.txt",title);
+  line = build_instructions(temp);
+  break;
+  case "servings":
+    temp = read_file("servings.txt",title);
+    line = temp.replace(/(\r\n|\n|\r|")/gm,"");
+  break;
+  case "time":
+    temp = read_file("time.txt",title);
+    line = build_time(temp);
+  break;
+  case "recipes":
+		line = build_recipes();
+  break;
+  case "reviews":
+  temp = read_file("reviews.txt",title);
+  line = build_Message(temp);
+  break;
+  default:
+    console.log("mode error");
+  break;
+}
+
+
+
+//an error case
+if(line == "") {
+  res.status(410);
+  res.send("recipe was not found");
+}
+
+res.send(JSON.stringify(line));
+
+});
+//post function to post to the files
+app.post('/', jsonParser, function (req, res) {
+	res.header("Access-Control-Allow-Origin", "*");
+	let name = req.body.name;
+  let folder = req.body.folder;
+	let comment = req.body.comment;
+  let foldername = "recipes/" +folder + "/reviews.txt";
+	console.log(name);
+	console.log(comment);
+  //string being sent into file
+	let string = "\n" + name + ": " + comment + "\n";
+	fs.appendFile(foldername, string, function(err) {
+    	if(err) {
+			console.log(err);
+			res.status(400);
+    	}
+    	console.log("The file was saved!");
+    	res.send("Success!");
+	});
+});
+
+app.listen(3000);
