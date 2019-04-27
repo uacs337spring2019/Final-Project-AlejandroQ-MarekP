81ee63a24df7e11a644ced22590f6fd479b89148
aquintana96 <47090413+aquintana96@users.noreply.github.com>
2019-04-26 18:12:57 -0700

    Update cooking_service.js

--- cooking_service.js
+++ cooking_service.js
@@ -12,6 +12,14 @@ app.use(function(req, res, next) {
 	next();
 });
 
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
+
 app.use(express.static('public'));
 app.use(express.static(__dirname));
 console.log("started");
